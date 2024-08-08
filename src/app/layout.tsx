0b706e05d49e7d8bbcaefc133bbec2b2a3068b04
GabrielVDantas <gvd2808@gmail.com>
2024-08-08 14:40:41 -0300

    Projeto base

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Nexus",
+  description: "Receba financiamento no Nexus!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
